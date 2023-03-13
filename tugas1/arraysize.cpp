3fdc4fd94ff3b8c3e058b1f3f1155a44b4332b00
ricomuh <rico.smartlolo@gmail.com>
2023-03-13 23:44:36 +0700

    edit array size

--- tugas1/arraysize.cpp
+++ tugas1/arraysize.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int arraySize(int arr[]) {
-    return sizeof(arr) / sizeof(arr[0]);
+// int arraySize(int arr[]) {
+//      return sizeof(arr) / sizeof(int); // error, it's not a pointer
+// }
+
+int vectorSize(vector<int> v) {
+    return v.size();
 }
 
 int main() {
     int a[5] = {1, 2, 3, 4, 5};
-    cout << "Size of array a: " << arraySize(a) << endl;
+    cout << "Size of array a: " << (sizeof(a) / sizeof(int)) << endl;
+    // cout << "Size of array a: " << arraySize(a) << endl;
+
+    vector<int> v = {1, 2, 3, 4, 5};
+    cout << "Size of vector v: " << v.size() << endl;
+    cout << "Size of vector v: " << vectorSize(v) << endl;
+
     return 0;
 }
